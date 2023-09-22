f48577377e183942a4635f7d84e44add4079f8ae
Sumon Chandra <topu199041@gmail.com>
2023-09-23 01:08:21 +0600

    Finish the first problem

---
+++ src/components/Problem-1.jsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+
+const Problem1 = () => {
+     const [show, setShow] = useState("all");
+     const [info, setInfo] = useState([]);
+
+     const handleSubmit = e => {
+          e.preventDefault();
+          const name = e.target.name.value;
+          const status = e.target.status.value;
+
+          setInfo(preInfo => [...preInfo, { name, status }]);
+     };
+
+     const handleClick = val => {
+          setShow(val);
+     };
+
+     info.sort((a, b) => {
+          const statusOrder = ["active", "completed", "all"];
+          const statusA = a.status.toLowerCase();
+          const statusB = b.status.toLowerCase();
+          return statusOrder.indexOf(statusA) - statusOrder.indexOf(statusB);
+     });
+
+     useEffect(() => {
+          setInfo(previousInfo => previousInfo.filter(item => item.status.toLowerCase() === show));
+     }, [show]);
+
+     return (
+          <div className="container">
+               <div className="row justify-content-center mt-5">
+                    <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+                    <div className="col-6 ">
+                         <form onSubmit={handleSubmit} className="row gy-2 gx-3 align-items-center mb-4">
+                              <div className="col-auto">
+                                   <input type="text" name="name" className="form-control" placeholder="Name" />
+                              </div>
+                              <div className="col-auto">
+                                   <input type="text" name="status" className="form-control" placeholder="Status" />
+                              </div>
+                              <div className="col-auto">
+                                   <button type="submit" className="btn btn-primary">
+                                        Submit
+                                   </button>
+                              </div>
+                         </form>
+                    </div>
+                    <div className="col-8">
+                         <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                              <li className="nav-item">
+                                   <button className={`nav-link ${show === "all" && "active"}`} type="button" onClick={() => handleClick("all")}>
+                                        All
+                                   </button>
+                              </li>
+                              <li className="nav-item">
+                                   <button
+                                        className={`nav-link ${show === "active" && "active"}`}
+                                        type="button"
+                                        onClick={() => handleClick("active")}
+                                   >
+                                        Active
+                                   </button>
+                              </li>
+                              <li className="nav-item">
+                                   <button
+                                        className={`nav-link ${show === "completed" && "active"}`}
+                                        type="button"
+                                        onClick={() => handleClick("completed")}
+                                   >
+                                        Completed
+                                   </button>
+                              </li>
+                         </ul>
+                         <div className="tab-content"></div>
+                         <table className="table table-striped ">
+                              <thead>
+                                   <tr>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Status</th>
+                                   </tr>
+                              </thead>
+                              <tbody>
+                                   {info?.map(item => (
+                                        <tr key={item.name}>
+                                             <td>{item.name}</td>
+                                             <td>{item.status}</td>
+                                        </tr>
+                                   ))}
+                              </tbody>
+                         </table>
+                    </div>
+               </div>
+          </div>
+     );
+};
+
+export default Problem1;
